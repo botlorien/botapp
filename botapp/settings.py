b27009d63533b1b08ec41ba24fcfd861a7ee80c5
Ben-Hur <botlorien@gmail.com>
2025-04-15 10:27:55 -0400

    static root

--- botapp/settings.py
+++ botapp/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
