d0e9fbeee01d8698fca83e625000e6b4c86bad4f
Ben-Hur <botlorien@gmail.com>
2025-04-15 10:34:31 -0400

    update settings

--- botapp/settings.py
+++ botapp/settings.py
@@ -71,10 +71,10 @@ TEMPLATES = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('BOTAPP_EMAIL_HOST')
