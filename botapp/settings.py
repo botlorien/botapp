1ed02fb940e3c613f4a01352ebb7a29bb3cb5beb
Carvalima Transportes <botlorien@gmail.com>
2025-04-15 11:19:26 -0400

    add servers

--- botapp/settings.py
+++ botapp/settings.py
@@ -42,6 +42,7 @@ DATABASES = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,6 +76,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('BOTAPP_EMAIL_HOST')
