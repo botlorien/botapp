a36920761dfc8216afad2da6a9d55c2a52615979
Carvalima Transportes <botlorien@gmail.com>
2025-04-17 17:47:27 -0400

    New variable for standalone mode

--- botapp/__init__.py
+++ botapp/__init__.py
@@ -1,10 +1,10 @@
 import os
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'botapp.settings')
-django.setup()
+# Só inicializa se for standalone
+if os.environ.get('BOTAPP_AUTOINIT', '1') == '1':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'botapp.settings')
+    import django
+    django.setup()
 
-from django.conf import settings  # <- agora é seguro
-
-# 4. Expõe o BotApp
+from django.conf import settings
 from .core import BotApp
